dd6f6ebb283677c2ad5b0e0531895d9ce5487104
Maul <maulanaammar101@gmail.com>
2025-05-03 13:29:50 +0700

    edit launch to workspace

--- vac_bot/src/vac_bot/launch/mapping.launch.py
+++ vac_bot/src/vac_bot/launch/mapping.launch.py
@@ -9,20 +9,20 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    pkg_bme_ros2_navigation = get_package_share_directory('bme_ros2_navigation')
+    pkg_name = get_package_share_directory(pkg_name)
 
-    gazebo_models_path, ignore_last_dir = os.path.split(pkg_bme_ros2_navigation)
+    gazebo_models_path, ignore_last_dir = os.path.split(pkg_name)
     os.environ["GZ_SIM_RESOURCE_PATH"] += os.pathsep + gazebo_models_path
 
-    rviz_launch_arg = DeclareLaunchArgument(
-        'rviz', default_value='true',
-        description='Open RViz'
-    )
+    # rviz_launch_arg = DeclareLaunchArgument(
+    #     'rviz', default_value='true',
+    #     description='Open RViz'
+    # )
 
-    rviz_config_arg = DeclareLaunchArgument(
-        'rviz_config', default_value='mapping.rviz',
-        description='RViz config file'
-    )
+    # rviz_config_arg = DeclareLaunchArgument(
+    #     'rviz_config', default_value='mapping.rviz',
+    #     description='RViz config file'
+    # )
 
     sim_time_arg = DeclareLaunchArgument(
         'use_sim_time', default_value='True',
@@ -44,21 +44,21 @@ def generate_launch_description():
     )
 
     slam_toolbox_params_path = os.path.join(
-        get_package_share_directory('bme_ros2_navigation'),
+        get_package_share_directory(pkg_name),
         'config',
         'slam_toolbox_mapping.yaml'
     )
 
-    # Launch rviz
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        arguments=['-d', PathJoinSubstitution([pkg_bme_ros2_navigation, 'rviz', LaunchConfiguration('rviz_config')])],
-        condition=IfCondition(LaunchConfiguration('rviz')),
-        parameters=[
-            {'use_sim_time': LaunchConfiguration('use_sim_time')},
-        ]
-    )
+    # # Launch rviz
+    # rviz_node = Node(
+    #     package='rviz2',
+    #     executable='rviz2',
+    #     arguments=['-d', PathJoinSubstitution([pkg_bme_ros2_navigation, 'rviz', LaunchConfiguration('rviz_config')])],
+    #     condition=IfCondition(LaunchConfiguration('rviz')),
+    #     parameters=[
+    #         {'use_sim_time': LaunchConfiguration('use_sim_time')},
+    #     ]
+    # )
 
     interactive_marker_twist_server_node = Node(
         package='interactive_marker_twist_server',
@@ -77,12 +77,5 @@ def generate_launch_description():
     )
 
     launchDescriptionObject = LaunchDescription()
-
-    launchDescriptionObject.add_action(rviz_launch_arg)
-    launchDescriptionObject.add_action(rviz_config_arg)
-    launchDescriptionObject.add_action(sim_time_arg)
-    launchDescriptionObject.add_action(rviz_node)
-    launchDescriptionObject.add_action(interactive_marker_twist_server_node)
-    launchDescriptionObject.add_action(slam_toolbox_launch)
 
     return launchDescriptionObject
